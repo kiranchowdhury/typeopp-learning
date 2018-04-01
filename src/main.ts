d882810ca37029a852f06e67d3d83dd7bb29e99d
Kiran Chowdhury <kiranchowdhury@Kirans-MacBook-Pro.local>
2018-04-01 12:04:38 +0530

    clean up

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { LicenseManager } from 'ag-grid-enterprise/main';
 
-LicenseManager.setLicenseKey('Evaluation_License_Valid_Until_16_May_2018__MTUyNjQyNTIwMDAwMA==816316e8c90ed402ff28dcf64cfe4646');
+LicenseManager.setLicenseKey('');
 if (environment.production) {
   enableProdMode();
 }
